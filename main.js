bd3c152e09ca18370c7e218fd2ee456cd4d4fa7a
peldas <13115959+peldas@users.noreply.github.com>
2024-07-16 17:52:42 +1000

    fix: attempt to re-inject whenever the page changes

--- main.js
+++ main.js
@@ -1,33 +1,32 @@
 // ==UserScript==
 // @name        Instagram Message List Toggle
 // @namespace   Violentmonkey Scripts
-// @match       *://www.instagram.com/direct/*
+// @match       *://www.instagram.com/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      peldas
-// @description 2024/7/9 18:18:57
+// @description 2024/7/16 17:50:00
 // @inject-into auto
 // ==/UserScript==
 var svg;
 function toggleMessageList() {
   console.log('trying to toggle...;')
-	messageList = document.querySelector('[role="navigation"]');
+  messageList = document.querySelector('div[role="navigation"]');
 
-	if (messageList.style.display === 'none') {
-		messageList.style.display = 'revert';
+  if (messageList.style.display === 'none') {
+    messageList.style.display = 'revert';
     svg.setAttribute('style', 'transform: rotate(90deg)');
-	} else {
-		messageList.style.display = 'none';
+  } else {
+    messageList.style.display = 'none';
     svg.setAttribute('style', 'transform: rotate(270deg)');
-	}
+  }
 }
-
-var injected = false;
 
 function injectSVG() {
   var a = document.createElement('a');
   a.onclick = function() { toggleMessageList(); };
   a.setAttribute('style', 'display:block;padding:0px 25px 0px 10px;');
+  a.setAttribute('role', 'messagelisthide');
 
   svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
   svg.setAttribute('fill', 'currentColor');
@@ -43,26 +42,26 @@ function injectSVG() {
   svg.appendChild(path);
   a.appendChild(svg);
 
-  var headerdiv = document.querySelector('[role="main"]').children[0].children[0].children[0].children[0].children[0];
+  var headerdiv = document.querySelector('div[role="main"]').children[0].children[0].children[0].children[0].children[0];
   headerdiv.insertBefore(a, headerdiv.children[0]);
-
-  injected = true;
 }
 
 var observer = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutation) {
-        if (!injected && mutation.type === 'childList') {
-            var navigationDiv = document.querySelector('[role="navigation"]');
-            var linkDiv = document.querySelector('[role="link"]');
-            var mainDiv = document.querySelector('[role="main"]');
+  mutations.forEach(function(mutation) {
+    var messageWindow = document.querySelector('div[role="main"]');
+    var listHideButton = document.querySelector('a[role="messagelisthide"]');
 
-            if (!navigationDiv || !linkDiv || !mainDiv)
-              return;
+    if (mutation.type !== 'childList') // only attempt to inject if the page has changed
+      return;
 
-            injectSVG();
-            observer.disconnect(); // Stop observing once injected
-        }
-    });
+    if (!messageWindow) // no need to inject if message window isn't visible
+      return;
+
+    if (listHideButton) // no need to inject if already injected
+      return;
+
+    injectSVG();
+  });
 });
 
 observer.observe(document.body, { subtree: true, childList: true });
